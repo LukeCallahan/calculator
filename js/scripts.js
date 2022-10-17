394cc8d21e26c2514eb290a589f9b51edc0167d2
LukeCallahan <lukemcal@gmail.com>
2022-10-17 14:37:31 -0700

    add all functions on one line

--- js/scripts.js
+++ js/scripts.js
@@ -9,6 +9,13 @@ function add(number1, number2) {
 function subtract(number1, number2) {
   return number1 - number2;
 }
+function multiply(number1, number2) {
+  return number1 * number2;
+}
+function divide(number1, number2) {
+  return number1 / number2;
+}
+
 
 
 
@@ -16,8 +23,12 @@ function subtract(number1, number2) {
 // user interface logic 
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
-//For addition
-window.alert("The sum is " + add(number1, number2));
 
-//For subtraction
-window.alert("The subtracted total is " + subtract(number1, number2));
+window.alert("The sum is " + add(number1, number2)+ "\nThe subtracted total is " + subtract(number1, number2) + "\nThe multiplied total is " + multiply(number1, number2) + "\nThe divided total is " + divide(number1, number2));
+
+const addAlert = number1 + " + " + number2 + " = " + add(number1, number2) + "\n" ;
+const subAlert = number1 + " - " + number2 + " = " + subtract(number1, number2) + "\n";
+const multAlert = number1 + " * " + number2 + " = " + multiply(number1, number2) + "\n";
+const divAlert = number1 + " / " + number2 + " = " + divide(number1, number2) + "\n";
+
+window.alert(addAlert + subAlert + multAlert + divAlert);
